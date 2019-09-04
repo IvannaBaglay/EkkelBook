ec42f1997c68e88d1842de90d720eb01807b60dc
IvannaB <ivankamorkai.baglay@gmail.com>
2019-09-04 20:55:31 +0300

    added class Foo

--- smart_pointers/all_function.h
+++ smart_pointers/all_function.h
@@ -1,1 +1,9 @@
-#pragma once
+#ifndef SMART_POINTERS_ALL_FUNCTION_H_
+#define SMART_POINTERS_ALL_FUNCTION_H_
+
+#include <iostream>
+#include <memory>
+
+void UseSharedPtr();
+
+#endif //SMART_POINTERS_ALL_FUNCTION_H_
