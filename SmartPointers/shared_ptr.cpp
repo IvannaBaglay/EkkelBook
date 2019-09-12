5b28375420df6f247f8cdaf6b24dac0dc1e8a9e6
IvannaB <ivankamorkai.baglay@gmail.com>
2019-09-12 21:31:13 +0300

    added task 12.1-12.2 lippman

--- SmartPointers/shared_ptr.cpp
+++ SmartPointers/shared_ptr.cpp
@@ -29,23 +29,69 @@ namespace {
     class StrBlob {
     public:
         typedef std::vector<std::string>::size_type size_type;
-        StrBlob() {
-
-        }
-        StrBlob(std::initializer_list<std::string> il) {
-
-        }
-        size_type size() const { return data->size; }
-        bool empty() const { return data->empty; }
+        StrBlob(): data(std::make_shared<std::vector<std::string>>()) { }
+        StrBlob(std::initializer_list<std::string> il):
+        data(std::make_shared<std::vector<std::string>>(il)) { }
+        size_type size() const { return data->size(); }
+        bool empty() const { return data->empty(); }
         void push_back(const std::string& t) { data->push_back(t); }
-        void pop_back();
-        std::string& front();
-        std::string& back();
+        void pop_back() {
+            check(0, "pop_back on empty StrBlob");
+            data->pop_back();
+        }
+        std::string& front() {
+            check(0, "front on empty StrBlob"); 
+            return data->front();
+        }
+        std::string& back() {
+            check(0, "back on empty StrBlob");
+            return data->back();
+        }
+        const std::string& front() const {
+            check(0, "front on empty StrBlob");
+            return data->front();
+        }
+        const  std::string& back() const {
+            check(0, "back on empty StrBlob");
+            return data->back();
+        }
     private:
         std::shared_ptr<std::vector<std::string>> data;
 
-        void check(size_type i, const std::string& msg) const;
+        void check(size_type i, const std::string& msg) const {
+            if (i >= data->size())
+                throw std::out_of_range(msg);
+        }
     };
+    void testStrBlob(StrBlob& sb) {
+        try {
+            sb.push_back("abc");
+            sb.push_back("def");
+            std::cout << "front: " << sb.front() << " back: " << sb.back() << std::endl;
+            sb.pop_back();
+            std::cout << "front: " << sb.front() << " back: " << sb.back() << std::endl;
+            sb.pop_back();
+            std::cout << "front: " << sb.front() << " back: " << sb.back() << std::endl;
+        }
+        catch (std::out_of_range err) {
+            std::cerr << err.what() << " out of range" << std::endl;
+        }
+        catch (std::exception err) {
+            std::cerr << err.what() << std::endl;
+        }
+    }
+
+    void testStrBlob(const StrBlob& sb) {
+        try {
+            std::cout << "front: " << sb.front() << " back: " << sb.back() << std::endl;
+        }
+        catch (std::out_of_range err) {
+            std::cerr << err.what() << " out of range" << std::endl;
+        }
+        catch (std::exception err) {
+            std::cerr << err.what() << std::endl;
+        }
+    }
 }
 
 
@@ -58,5 +104,39 @@ void UseSharedPtr() {
     auto foo2(foo);
     r = q;  
     foo = UseFactoryWithReturn(5);
+
+    //12.1
+    {
+        StrBlob b1;
+        {
+            StrBlob b2 = { "a", "an", "the" };
+            b1 = b2;
+            b2.push_back("about");
+        }
+
+    }
+
+    //12.2
+    {
+        StrBlob sb1;
+        StrBlob sb2{ "Hello", "World" };
+        StrBlob sb3 = { "Bye", "World" };
+        const StrBlob csb1;
+        const StrBlob csb2{ "This", "Blob" };
+
+        testStrBlob(sb1);
+        std::cout << std::endl;
+        testStrBlob(sb2);
+        std::cout << std::endl;
+        testStrBlob(sb3);
+        std::cout << std::endl;
+        testStrBlob(csb1);
+        std::cout << std::endl;
+        testStrBlob(csb2);
+        std::cout << std::endl;
+        testStrBlob({ "ppp", "qqq" });
+        std::cout << std::endl;
+        //testStrBlob({"mm", 1});  // Error
+    }
 
 }
