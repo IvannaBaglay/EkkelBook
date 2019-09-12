cc1bddf466295101e5e764f66f883877fb1a81e1
IvannaB <ivankamorkai.baglay@gmail.com>
2019-09-12 20:07:11 +0300

    added class StrBlob

--- SmartPointers/shared_ptr.cpp
+++ SmartPointers/shared_ptr.cpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include <string>
+
 #include "all_function.h"
 
 namespace {
@@ -22,7 +25,29 @@ namespace {
         std::shared_ptr<Foo> p = Factory(arg);
         return p; // increment pointer's count 
     }//  don't delete object 
+
+    class StrBlob {
+    public:
+        typedef std::vector<std::string>::size_type size_type;
+        StrBlob() {
+
+        }
+        StrBlob(std::initializer_list<std::string> il) {
+
+        }
+        size_type size() const { return data->size; }
+        bool empty() const { return data->empty; }
+        void push_back(const std::string& t) { data->push_back(t); }
+        void pop_back();
+        std::string& front();
+        std::string& back();
+    private:
+        std::shared_ptr<std::vector<std::string>> data;
+
+        void check(size_type i, const std::string& msg) const;
+    };
 }
+
 
 void UseSharedPtr() {
     auto p = std::make_shared<int>(42);
@@ -32,6 +57,6 @@ void UseSharedPtr() {
     auto foo = std::make_shared<Foo>(1);
     auto foo2(foo);
     r = q;  
-    foo = UseFactoryWithReturn(1);
+    foo = UseFactoryWithReturn(5);
 
 }
