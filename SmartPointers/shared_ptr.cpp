20966cadc0c2f0e8534330d7db3c0ab5d4b42639
IvannaB <ivankamorkai.baglay@gmail.com>
2019-09-14 17:37:00 +0300

    added files for classes StrBlob and StrBlobPtr

--- SmartPointers/shared_ptr.cpp
+++ SmartPointers/shared_ptr.cpp
@@ -1,7 +1,4 @@
-#include <vector>
-#include <string>
-
-#include "all_function.h"
+#include "StrBlob.h"
 
 namespace {
 
@@ -26,43 +23,7 @@ namespace {
         return p; // increment pointer's count 
     }//  don't delete object 
 
-    class StrBlob {
-    public:
-        typedef std::vector<std::string>::size_type size_type;
-        StrBlob(): data(std::make_shared<std::vector<std::string>>()) { }
-        StrBlob(std::initializer_list<std::string> il):
-        data(std::make_shared<std::vector<std::string>>(il)) { }
-        size_type size() const { return data->size(); }
-        bool empty() const { return data->empty(); }
-        void push_back(const std::string& t) { data->push_back(t); }
-        void pop_back() {
-            check(0, "pop_back on empty StrBlob");
-            data->pop_back();
-        }
-        std::string& front() {
-            check(0, "front on empty StrBlob"); 
-            return data->front();
-        }
-        std::string& back() {
-            check(0, "back on empty StrBlob");
-            return data->back();
-        }
-        const std::string& front() const {
-            check(0, "front on empty StrBlob");
-            return data->front();
-        }
-        const  std::string& back() const {
-            check(0, "back on empty StrBlob");
-            return data->back();
-        }
-    private:
-        std::shared_ptr<std::vector<std::string>> data;
-
-        void check(size_type i, const std::string& msg) const {
-            if (i >= data->size())
-                throw std::out_of_range(msg);
-        }
-    };
+    
     void testStrBlob(StrBlob& sb) {
         try {
             sb.push_back("abc");
