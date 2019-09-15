86db76620c8dc44e3d2d9def55d413e7fff79286
IvannaB <ivankamorkai.baglay@gmail.com>
2019-09-15 17:06:55 +0300

    added new solution for task

--- SmartPointers/all_function.h
+++ SmartPointers/all_function.h
@@ -6,5 +6,9 @@
 
 void UseSharedPtr();
 void UseUniqurPtr();
+int task2();
+int task3();
+int task4();
+int task5();
 
 #endif //SMART_POINTERS_ALL_FUNCTION_H_
---
+++ SmartPointers/task2.cpp
@@ -0,0 +1,46 @@
+/*
+    Polymorphism and virtual functions
+    Conversions
+    STL containers
+    STL iterators
+*/
+
+#include <iostream>
+#include <vector>
+
+namespace {
+    class A {
+    public:
+        A(int n = 0) : m_n(n) {
+
+        }
+
+        virtual int f() const { return m_n; }
+        virtual ~A() {  };
+
+    protected:
+        int m_n;
+    };
+
+    class B : public A {
+    public:
+        B(int n = 0) : A(n) {
+
+        }
+
+        int f() const override { return m_n + 1; }
+    };
+}
+
+int task2() {
+    const A a(1);
+    const B b(3);
+    const A* x[2] = { &a, &b };
+    typedef std::vector<A> V;
+    V y({ a, b });
+    V::const_iterator i = y.begin();
+
+    std::cout << x[0]->f() << x[1]->f() << i->f() << (i + 1)->f() << std::endl;
+
+    return 0;
+}
