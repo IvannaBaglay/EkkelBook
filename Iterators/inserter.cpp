0205fd56681db98b411bc44770d8077c16f40669
IvannaB <ivankamorkai.baglay@gmail.com>
2019-10-03 16:06:10 +0300

    Added example unique

--- Iterators/inserter.cpp
+++ Iterators/inserter.cpp
@@ -1,7 +1,13 @@
 #include <list>
 #include <iterator>
+#include <vector>
+#include <algorithm>
 
 #include "all_function.h"
+
+bool myfunction(int i, int j) {
+    return (i == j);
+}
 
 int inserter() {
 
@@ -11,6 +17,58 @@ int inserter() {
     copy(lst.begin(), lst.end(), front_inserter(lst2));
 
     copy(lst.begin(), lst.end(), inserter(lst3, lst3.begin()));
+
+    return 0;
+}
+
+int function_unique() {
+
+    {
+        int myints[] = { 10,20,20,20,30,30,20,20,10 };           // 10 20 20 20 30 30 20 20 10
+        std::vector<int> myvector(myints, myints + 9);
+
+        // using default comparison:
+        std::vector<int>::iterator it;
+        it = std::unique(myvector.begin(), myvector.end());   // 10 20 30 20 10 ?  ?  ?  ?
+                                                               //                ^
+
+        myvector.resize(std::distance(myvector.begin(), it)); // 10 20 30 20 10
+
+        // using predicate comparison:
+        std::unique(myvector.begin(), myvector.end(), myfunction);   // (no changes)
+
+        // print out content:
+        std::cout << "myvector contains:";
+        for (it = myvector.begin(); it != myvector.end(); ++it)
+            std::cout << ' ' << *it;
+        std::cout << '\n';
+    }
+
+    {
+        int myints[] = { 10,20,20,20,30,30,20,20,10 };
+        std::vector<int> myvector(9);                            // 0  0  0  0  0  0  0  0  0
+
+        // using default comparison:
+        std::vector<int>::iterator it;
+        it = std::unique_copy(myints, myints + 9, myvector.begin());   // 10 20 30 20 10 0  0  0  0
+                                                                  //                ^
+
+        std::sort(myvector.begin(), it);                          // 10 10 20 20 30 0  0  0  0
+                                                                  //                ^
+
+        // using predicate comparison:
+        it = std::unique_copy(myvector.begin(), it, myvector.begin(), myfunction);
+        // 10 20 30 20 30 0  0  0  0
+        //          ^
+
+        myvector.resize(std::distance(myvector.begin(), it));    // 10 20 30
+
+        // print out content:
+        std::cout << "myvector contains:";
+        for (it = myvector.begin(); it != myvector.end(); ++it)
+            std::cout << ' ' << *it;
+        std::cout << '\n';
+    }
 
     return 0;
 }
