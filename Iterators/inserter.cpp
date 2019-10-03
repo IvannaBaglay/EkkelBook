c02b0b6dce6b888c5f312a7b29d555533e0e372b
IvannaB <ivankamorkai.baglay@gmail.com>
2019-10-03 16:09:56 +0300

    Added task 10.28

--- Iterators/inserter.cpp
+++ Iterators/inserter.cpp
@@ -2,6 +2,7 @@
 #include <iterator>
 #include <vector>
 #include <algorithm>
+#include <deque>
 
 #include "all_function.h"
 
@@ -17,6 +18,36 @@ int inserter() {
     copy(lst.begin(), lst.end(), front_inserter(lst2));
 
     copy(lst.begin(), lst.end(), inserter(lst3, lst3.begin()));
+
+    {
+        std::vector<int> source{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        std::vector<int> ivec;
+        std::deque<int> ideq;
+        std::list<int> ilist;
+
+        // back_inserter: output 1 2 3 4 5 6 7 8 9
+        copy(source.cbegin(), source.cend(), back_inserter(ivec));
+        // front inserter: output 9 8 7 6 5 4 3 2 1
+        copy(source.cbegin(), source.cend(), front_inserter(ideq));
+        // inserter: output 1 2 3 4 5 6 7 8 9
+        copy(source.cbegin(), source.cend(), inserter(ilist, ilist.begin()));
+
+        std::cout << "back_inserter and vector: ";
+        for (const auto& elem : ivec)
+            std::cout << elem << ' ';
+        std::cout << '\n';
+
+        std::cout << "front_inserter and deque: ";
+        for (const auto& elem : ideq)
+            std::cout << elem << ' ';
+        std::cout << '\n';
+
+        std::cout << "inserter and list: ";
+        for (const auto& elem : ilist)
+            std::cout << elem << ' ';
+        std::cout << '\n';
+
+    }
 
     return 0;
 }
