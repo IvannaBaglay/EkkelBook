e26cf17b2f023b382d679f9714c81987ac43c8ad
IvannaB <ivankamorkai.baglay@gmail.com>
2019-10-03 13:44:13 +0300

    Added example with difference capacity in for_each()

--- lambda_express/lambda_capture_implicitly.cpp
+++ lambda_express/lambda_capture_implicitly.cpp
@@ -11,8 +11,19 @@ int ImplicitlyLambdaCapture() {
         words.push_back("aa");
         words.push_back("aaa");
         int sz = 2;
+        std::ostream& os = std::cout;
+        char c = ' ';
         auto wc = find_if(words.begin(), words.end(), [=](const std::string& a)
             {return a.size() >= sz; });
+
+        //os implicitly capture by reference
+        // c capture by value 
+        for_each(words.begin(), words.end(), [&, c](const std::string& s)
+            {os << s << c; });
+        // os capture by reference
+        // c implicitly capture by value
+        for_each(words.begin(), words.end(), [=, &os](const std::string& s)
+            {os << s << c; });
         
     }
 
