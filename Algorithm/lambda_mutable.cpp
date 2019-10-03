a7f1628b09ebfe9453fd09faab6731e76b6116e6
IvannaB <ivankamorkai.baglay@gmail.com>
2019-10-03 14:01:33 +0300

    Added example with keyword mutable in lambda

--- Algorithm/lambda_mutable.cpp
+++ Algorithm/lambda_mutable.cpp
@@ -5,5 +5,23 @@
 
 int LambdaMutable() {
 
+    {
+        size_t v1 = 42;
+        // auto f = [v1]() { return ++v1; }; //error
+        v1 = 0;
+        //auto j = f();
+    }
+    {
+        size_t v1 = 42;
+        auto f = [v1]() mutable { return ++v1; };
+        v1 = 0;
+        auto j = f(); // j = 43
+    }
+    {
+        size_t v1 = 42;
+        auto f = [&v1]() mutable { return ++v1; };
+        v1 = 0;
+        auto j = f(); // j = 1
+    }
     return 0;
 }
